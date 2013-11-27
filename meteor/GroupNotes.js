c61c513394fba73b83c98e84553a0f6cf6c635e3
Thomas Hansen <th4019@gmail.com>
2013-11-27 10:14:06 -0700

    initial meteor project

---
+++ meteor/GroupNotes.js
@@ -0,0 +1,51 @@
+//var Universities = Meteor.Collections("universities");
+
+if (Meteor.isClient) {
+
+  Router.configure({
+    autoRender: false,
+    notFoundTemplate: 'notFound',
+  });
+
+  Router.map(function () {
+    this.route('home', {
+      path: '/',
+    });
+
+    this.route('home-user', {
+      path: '/home-user',
+    });
+
+    this.route('contact', {
+      path: '/contact',
+    });
+
+    this.route('about-us', {
+      path: '/about-us',
+      template: 'about_us',
+    });
+  });
+
+  Template.noteFinder.universities = function (today) {
+    return Universities.find();
+  };
+
+  Template.noteFinder.rendered = function() {
+    $('#university').select2({
+        minimumInputLength: 1,
+        query: function (query) {
+            var data = {results: []}, i, j, s;
+            data.results.push({id: 'byu', text: "Brigham Young University"})
+            data.results.push({id: 'uvu', text: "Utah Valley University"})
+            data.results.push({id: 'u of u', text: "University of Utah"})
+            query.callback(data);
+        }
+    });
+  };
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
