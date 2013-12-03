88d2bf789d6b5c4b2e700b2a120cf337aa0d0cc9
Tim Price <malikazoo@gmail.com>
2013-12-02 19:15:43 -0700

    Added mongo login and fixed a few bugs in deploy.
    Told deploy to run at port 2000 instead of 3000
    bundle uses mrt instead of directly meteor to ensure bundling of all packages

--- meteor/deploy.sh
+++ meteor/deploy.sh
@@ -3,11 +3,11 @@
 #bundle and deploy
 forever stop /var/www/deployed/bundle/main.js
 rm -rf /var/www/deployed/bundle
-meteor bundle /var/www/deployed/groupnotes.tgz
+mrt bundle /var/www/deployed/groupnotes.tgz
 tar -C /var/www/deployed -xf /var/www/deployed/groupnotes.tgz 
 
 #start the server
 export ROOT_URL='http://groupnotes.org'
-export PORT='3000'
+export PORT='2000'
 export MONGO_URL='mongodb://gn:groupnotes@localhost/groupnotes'
 forever start /var/www/deployed/bundle/main.js
