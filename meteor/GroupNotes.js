982468639e945ec7e7402cd7d9a15b15e91ca673
Thomas Hansen <th4019@gmail.com>
2013-12-10 19:39:02 +0000

    dropdown changes

--- meteor/GroupNotes.js
+++ meteor/GroupNotes.js
@@ -1,4 +1,5 @@
-//var Universities = Meteor.Collections("universities");
+var Universities = new Meteor.Collection("universities");
+var Professors = new Meteor.Collection("professors");
 
 if (Meteor.isClient) {
   Router.configure({
@@ -33,12 +34,41 @@ if (Meteor.isClient) {
 
   Template.noteFinder.rendered = function() {
     $('#university').select2({
-        minimumInputLength: 1,
+        dropDownCssClass: 'noteFinderDrop',
         query: function (query) {
             var data = {results: []}, i, j, s;
-            data.results.push({id: 'byu', text: "Brigham Young University"})
-            data.results.push({id: 'uvu', text: "Utah Valley University"})
-            data.results.push({id: 'u of u', text: "University of Utah"})
+            var regex = new RegExp(query.term, 'i');
+            data.results = Universities.find({$or : [{'name' : regex}, {'acronym' : regex}] }).fetch();
+            data.results.forEach(function(element, index, array) {
+              element.id = element._id._str;
+              element.text = element.acronym + ' - ' + element.name;
+            });
+            query.callback(data);
+        }
+    });
+    $('#university').change(function() {
+      $('#teacher').removeAttr('disabled');
+      $('#teacher').select2("enable", true);
+    });
+    $('#teacher').select2({
+        query: function (query) {
+            var data = {results: []}, i, j, s;
+            var regex = new RegExp(query.term, 'i');
+            console.log($('#university').val());
+            var university_query = { 'university' : new Meteor.Collection.ObjectID($('#university').val()) };
+            var name_query = {$or : [ {'first_name' : regex}, {'last_name' : regex} ]};
+            data.results = Professors.find({$and : [name_query, university_query]}).fetch();
+            data.results.forEach(function(element, index, array) {
+              element.id = element._id._str;
+              element.text = element.first_name + ' - ' + element.last_name;
+            });
+            query.callback(data);
+        }
+    });
+    $('#class').select2({
+        query: function (query) {
+            var data = {results: []}, i, j, s;
+            var regex = new RegExp(query.term, 'i');
             query.callback(data);
         }
     });
