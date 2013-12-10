a98b18665456d338c90d560cb62f0980208c8cad
Thomas Hansen <th4019@gmail.com>
2013-12-10 21:01:17 +0000

    links to add new classes or professors

--- meteor/GroupNotes.js
+++ meteor/GroupNotes.js
@@ -1,5 +1,6 @@
 var Universities = new Meteor.Collection("universities");
 var Professors = new Meteor.Collection("professors");
+var Classes = new Meteor.Collection("classes");
 
 if (Meteor.isClient) {
   Router.configure({
@@ -33,8 +34,16 @@ if (Meteor.isClient) {
   };
 
   Template.noteFinder.rendered = function() {
+    var university_name = amplify.store('university-name');
+    if (!amplify.store('university')) {
+      $('#teacher').select2("enable", false);
+      $('#class').select2("enable", false);
+    }
+    $('#university').attr('placeholder', university_name);
+
     $('#university').select2({
         dropDownCssClass: 'noteFinderDrop',
+        placeholder: amplify.store('university') ? amplify.store('university') : 'university',
         query: function (query) {
             var data = {results: []}, i, j, s;
             var regex = new RegExp(query.term, 'i');
@@ -46,17 +55,12 @@ if (Meteor.isClient) {
             query.callback(data);
         }
     });
-    $('#university').change(function() {
-      $('#teacher').removeAttr('disabled');
-      $('#teacher').select2("enable", true);
-      $('#class').select2("enable", true);
-    });
+
     $('#teacher').select2({
         query: function (query) {
             var data = {results: []}, i, j, s;
             var regex = new RegExp(query.term, 'i');
-            console.log($('#university').val());
-            var university_query = { 'university' : new Meteor.Collection.ObjectID($('#university').val()) };
+            var university_query = { 'university' : new Meteor.Collection.ObjectID(amplify.store('university')) };
             var name_query = {$or : [ {'first_name' : regex}, {'last_name' : regex} ]};
             data.results = Professors.find({$and : [name_query, university_query]}).fetch();
             data.results.forEach(function(element, index, array) {
@@ -66,12 +70,39 @@ if (Meteor.isClient) {
             query.callback(data);
         }
     });
+
     $('#class').select2({
         query: function (query) {
             var data = {results: []}, i, j, s;
             var regex = new RegExp(query.term, 'i');
+            var university_query = { 'university' : new Meteor.Collection.ObjectID(amplify.store('university')) };
+            var name_query = {'name' : regex};
+            data.results = Classes.find({$and : [name_query, university_query]}).fetch();
+            data.results.forEach(function(element, index, array) {
+              element.id = element._id._str;
+              element.text = element.name;
+            });
             query.callback(data);
         }
+    });
+
+    $('.select2-drop').each( function( index, element ) {
+      if (index == 1) {
+        $(element).append('<div class="add"><span class="glyphicon glyphicon-plus"></span> Add new professor</div>');
+      }
+      if (index == 2) {
+        $(element).append('<div class="add"><span class="glyphicon glyphicon-plus"></span> Add new class</div>');
+      }
+      console.log(element);
+    });
+
+    $('#university').change(function() {
+      $('#teacher').select2("enable", true);
+      $('#class').select2("enable", true);
+      $('#class').select2("val", '');
+      $('#teacher').select2("val", '');
+      amplify.store('university', $(this).val() );
+      amplify.store('university-name', $('#university').select2("data").text);
     });
   };
 }
